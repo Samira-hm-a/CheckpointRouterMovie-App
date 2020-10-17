393343ef07614bef82b0b882ec4ae5d1d2ffee36
Samirahm <58561180+Samira-hm-a@users.noreply.github.com>
2020-10-17 23:47:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
-import React, { useState }  from "react";
-import Header from "./Components/Navbar/Header";
-import MovieList from "./Components/MovieList";
-import SearchMovie from "./Components/SearchMovie";
-import { movieContainer } from "./Components/MovieContainer";
-import AddMovie from "./Components/Modal/AddMovie";
-import "./App.css";
-import Footer from "./Components/Footer/Footer";
+import React from 'react'
+import Home from './Home'
+import {Route,Switch,Link } from 'react-router-dom';
 import MovieTrailer from './Components/MovieTrailer';
-import { Route, Switch } from "react-router-dom";
-import  Home from "./Components/Home";
+import "./App.css";
 
-
-function App(props) {
-  const [movieData, setMovieData] = useState(movieContainer);
-  const [movieSearch, setMovieSearch] = useState("");
-  const [rateMovie, setRateMovie] = useState(1);
-  const adding = (e, newMovie) => {
-    e.preventDefault();
-    setMovieData([...movieData, newMovie]);
-  };
-    return (
-    <div className="App">
-      <h1>
-      <Header text="MOVIE APP" /></h1>
-      <div style={{ cursor: "pointer" }}>
-        <SearchMovie
-          setMovieSearch={setMovieSearch}
-          rateMovie={rateMovie}
-          setRateMovie={setRateMovie}
-        />
-      </div>
-      <AddMovie adding={adding} />
-      <MovieList movieData={movieData} movieSearch={movieSearch} rateMovie={rateMovie} />
-      <>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path="/:id" component={MovieTrailer}/>
-      </Switch>
-      </>
-      <Footer/>
-    </div>
-  );
+export default function App(){
+  return(
+    <>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path="/:id" component={MovieTrailer}/>
+    </Switch>
+    </>
+  )
 }
-
-export default App;
